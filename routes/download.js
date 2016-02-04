58614d821073ac4f6909875ea86c4b7507df1f08
dourgulf <dourgulf@gmail.com>
2016-02-04 14:37:18 +0800

    make more output message

--- routes/download.js
+++ routes/download.js
@@ -53,15 +53,24 @@ router.get('/', function (req, res) {
     debug("appID:" + appID);
     debug("userID:" + userID);
     dbservice.queryAppInfo(appID, userID, function (err, appInfo) {
+        if (err) {
+            res.render('error', {
+                message: "Can't query this app:" + appID,
+                error: error
+            });
+            return ;
+        }
+
         appInfo.basePath = path.join(baseFilePath, appInfo.storageID);
         var buildInfo = appInfo.version + "(" + appInfo.buildVersion + ")";
         var iconPath = path.join(appInfo.basePath, "/icon.png");
+        var contentParams = {AppTitle: appInfo.title, BuildInfo: buildInfo}
         fs.readFile(iconPath, function (err, iconData) {
             if (!err) {
-                var iconDataString = "data:image/png;base64," + iconData.toString("base64");
-                var htmlContent = installHTMLTemplate.coolFormat({AppTitle: appInfo.title, BuildInfo: buildInfo, iconURL: iconDataString});
-                res.send(htmlContent);
+                contentParams.iconURL = "data:image/png;base64," + iconData.toString("base64");
             }
+            var htmlContent = installHTMLTemplate.coolFormat({AppTitle: appInfo.title, BuildInfo: buildInfo, iconURL: iconDataString});
+            res.send(htmlContent);
         });
 
     })
